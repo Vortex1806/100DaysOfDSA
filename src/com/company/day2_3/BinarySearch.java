f19bd300053df7002b5db073f43318ffb6987452
Vortex1806 <shubhvora03@gmail.com>
2022-06-07 09:45:09 +0530

    Day 2 Starting Binary Search

---
+++ src/com/company/day2_3/BinarySearch.java
@@ -0,0 +1,5 @@
+package com.company.day2_3;
+
+public class BinarySearch {
+
+}
--- src/com/company/fibonum.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class fibonum {
-    public static void main(String[] args) {
-
-    }
-}
